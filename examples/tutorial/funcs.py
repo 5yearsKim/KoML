b1a5eea81b131536e84cd4d7c7800b8df022f582
5yearsKim <hypothesis22@naver.com>
2022-11-24 21:26:47 +0900

    adding plus example

--- examples/tutorial/funcs.py
+++ examples/tutorial/funcs.py
@@ -11,6 +11,15 @@ def show_date(context=None):
         am_pm = '오전'
     return f'{am_pm} {h}시 {m}분'
 
+
+def plus(a: str, b: str, context=None) -> str:
+    try:
+        a_num = int(a)
+        b_num = int(b)
+        return str(a_num + b_num)
+    except:
+        return '???'
+
 def know(key: str, context= None) -> str:
     if key in context.memo:
         return 'true'
--- examples/tutorial/main.py
+++ examples/tutorial/main.py
@@ -4,15 +4,15 @@ sys.path.append('../../')
 
 # main.py
 from koml import KomlBot, CustomBag
-from funcs import show_date, know
+from funcs import show_date, know, plus
 
 
 # bot = KomlBot()
 # bot.learn(['section.xml', 'tags.xml', 'template.xml'])
 # bot.converse()
 
-# bag = CustomBag(funcs={'show_date': show_date, 'know': know})
-bag = CustomBag(funcs={'know': know})
+bag = CustomBag(funcs={'show_date': show_date, 'know': know, 'plus': plus})
+# bag = CustomBag(funcs={'know': know})
 
 bot = KomlBot(bag)
 bot.learn(['section.xml', 'tags.xml', 'template.xml', 'custom_function.xml'])
